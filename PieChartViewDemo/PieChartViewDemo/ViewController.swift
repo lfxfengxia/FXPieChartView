67f2ec9e42ba3fed4be79465e3df98f1426dd3b6
lfx <sayingwhy@163.com>
2017-07-05 17:14:21 +0800

    修改调用的备注

--- PieChartViewDemo/PieChartViewDemo/ViewController.swift
+++ PieChartViewDemo/PieChartViewDemo/ViewController.swift
@@ -15,9 +15,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         let pieView = FXPieChartView(frame: CGRect(x: 0, y: 150, width: view.frame.size.width, height: view.frame.size.width * 0.8))
-        pieView.percentAcoordingToRadius = false;//true 效果图1 false 效果图2
 //        pieView.valueArr = [10,19,23,25,8,20,15,12] //设置该属性 没有标注 
         pieView.dataDic = ["英国" : 10,"美国" : 19,"中国" : 23,"韩国" : 25,"日本" : 8,"德国" : 20,"西班牙" : 15,"葡萄牙" : 12]//设置国家名称以及对应的实际数据，不是所占比例（设置该属性有标注）
+//        pieView.percentAcoordingToRadius = false;//true 效果图1 false 效果图2 默认为true
         view.addSubview(pieView)
     }
 
